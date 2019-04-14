55fe761c9390c849503f4be6fd6f9d3f126c0e77
Kristin Kraemer <kristinhannahk@gmail.com>
2019-04-14 22:51:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(int)
+  if int <= 1
+    return false 
+  elsif int == 2 
+    return true
+  else 
+  end
+  n = 2
+  until n == int - 1 
+  if int % n == 0
+    return false
+  n += 1
+  break if n == int
+end
+return true
+end
+end
